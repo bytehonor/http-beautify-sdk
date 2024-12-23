def4b688f02fb93e4a7fbc4c3397603e748b78c8
lijianqiang <newphper@126.com>
2024-12-23 10:32:39 +0800

    1.3.1

--- src/main/java/com/bytehonor/sdk/beautify/http/config/HttpConfig.java
+++ src/main/java/com/bytehonor/sdk/beautify/http/config/HttpConfig.java
@@ -2,7 +2,7 @@ package com.bytehonor.sdk.beautify.http.config;
 
 public class HttpConfig {
 
-    private static final String USER_AGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1";
+    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36";
 
     private static final int MAX_IDLE = 10;
 
